000a1a963022ef0119c87abb89f2f745fc9ccf05
Bill S <bill.e.seaman@gmail.com>
2018-10-28 17:37:37 -0700

    Initial Commit

---
+++ MVVM_Rx_CoreData_TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVM_Rx_CoreData_TestProject
+//
+//  Created by Loud on 10/28/18.
+//  Copyright © 2018 William Seaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
